179acbbe15496a31c8d9eec74be0c8ae4ffa341b
seitamuro <seitamuro0210@gmail.com>
2023-07-09 20:57:07 +0900

    load king sprite

--- src/scenes/loading/index.ts
+++ src/scenes/loading/index.ts
@@ -1,11 +1,19 @@
-import { Scene } from 'phaser';
+import { GameObjects, Scene } from 'phaser';
 
 export class LoadingScene extends Scene {
+  private king!: GameObjects.Sprite;
+
   constructor() {
     super("loading-scene");
   }
 
   create(): void {
     console.log("Loading scene was created");
+    this.king = this.add.sprite(100, 100, "king");
+  }
+
+  preload(): void {
+    this.load.setBaseURL("/assets/");
+    this.load.image("king", "sprites/king.png");
   }
 }
