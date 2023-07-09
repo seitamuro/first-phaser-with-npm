8914674d1e36cd61940c31d408c0ad10bbedb61d
seitamuro <seitamuro0210@gmail.com>
2023-07-09 23:38:35 +0900

    apply Player class

--- src/scenes/level-1/index.ts
+++ src/scenes/level-1/index.ts
@@ -1,13 +1,18 @@
 import { GameObjects, Scene } from "phaser";
+import { Player } from "@/classes/player";
 
 export class Level1 extends Scene {
-  private king!: GameObjects.Sprite;
+  private player!: Player;
 
   constructor() {
     super("level-1-scene")
   }
 
   create(): void {
-    this.king = this.add.sprite(100, 100, "king")
+    this.player = new Player(this, 100, 100);
+  }
+
+  update(): void {
+    this.player.update();
   }
 }
