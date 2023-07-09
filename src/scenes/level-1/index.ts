803b7a855a15c9f55acba00c4371650d5e0990eb
seitamuro <seitamuro0210@gmail.com>
2023-07-10 04:11:37 +0900

    add collision

--- src/scenes/level-1/index.ts
+++ src/scenes/level-1/index.ts
@@ -15,6 +15,7 @@ export class Level1 extends Scene {
   create(): void {
     this.initMap();
     this.player = new Player(this, 100, 100);
+    this.physics.add.collider(this.player, this.wallsLayer);
   }
 
   update(): void {
@@ -26,6 +27,16 @@ export class Level1 extends Scene {
     this.tileset = this.map.addTilesetImage('dungeon', 'tiles')!;
     this.groundLayer = this.map.createLayer('Ground', this.tileset, 0, 0)!;
     this.wallsLayer = this.map.createLayer('Wall', this.tileset, 0, 0)!;
+    this.wallsLayer.setCollisionByProperty({ collides: true });
     this.physics.world.setBounds(0, 0, this.wallsLayer.width, this.wallsLayer.height);
+    this.showDebugWalls();
+  }
+
+  private showDebugWalls(): void {
+    const debugGraphics = this.add.graphics().setAlpha(0.75);
+    this.wallsLayer.renderDebug(debugGraphics, {
+      tileColor: null,
+      collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
+    })
   }
 }
